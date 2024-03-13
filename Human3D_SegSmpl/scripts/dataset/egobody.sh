7acdd859dd3024161396219162f3e1f1e6734c08
WYZ <98224689+WeiyueZ@users.noreply.github.com>
2024-03-13 14:45:04 +0800

    Delete Human3D_SegSmpl directory

--- Human3D_SegSmpl/scripts/dataset/egobody.sh
+++
@@ -1,4 +0,0 @@
-python humanseg_preprocessing.py preprocess \
---data_dir="../../data/raw/egobody_dif" \
---save_dir="../../data/processed/egobody" \
---dataset="egobody"
--- Human3D_SegSmpl/scripts/dataset/synthetic_humans.sh
+++
@@ -1,6 +0,0 @@
-python datasets/preprocessing/humanseg_preprocessing.py preprocess \
---data_dir="../../data/raw/synthetic_humans" \
---save_dir="../../data/processed/synthetic_humans" \
---dataset="synthetic_humans" \
---min_points=20000 \
---min_instances=1
